963994c7e245c413b075c299affb48cc19a88578
Nam <nambcvt275@gmail.com>
2023-03-28 05:04:38 +0700

    optimize: types

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -11,7 +11,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/2751997nam/go-helpers/internal/types"
+	"github.com/2751997nam/go-helpers/pkg/types"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/exp/maps"
