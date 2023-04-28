806f88024212a0afca9ea63cfea87cfbad81a1a4
Nam <nambcvt275@gmail.com>
2023-04-28 16:59:32 +0700

    optimize: grpc response

--- utils/response.go
+++ utils/response.go
@@ -1,8 +1,9 @@
 package utils
 
 type Response struct {
-	Status  string `json:"status"`
-	Result  any    `json:"result"`
-	Meta    any    `json:"meta,omitempty"`
-	Message string `json:"message"`
+	Status     string `json:"status"`
+	Result     any    `json:"result"`
+	Meta       any    `json:"meta,omitempty"`
+	Message    string `json:"message"`
+	StatusCode int32  `json:"status_code,omitempty"`
 }
